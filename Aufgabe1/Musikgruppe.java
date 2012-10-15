e34df39fd3a0dab9eb45c317626fdb6d3621ccb1
Johannes Wawerda <johannes.wawerda@studten.tuwien.ac.at>
2012-10-15 21:12:26 +0200

    Termin, Auftritt, Probe und Termincontainer erstelle. Musikgruppe bearbeitet. Test bearbeitet

--- Aufgabe1/Musikgruppe.java
+++ Aufgabe1/Musikgruppe.java
@@ -6,9 +6,47 @@ package Aufgabe1;
 
 /**
  *
- * @author Jotschi
+ * @author Johannes Wawerda <johannes.wawerda@student.tuwien.ac.at>
  */
-public class Musikgruppe  {
+public class Musikgruppe {
+    private Termincontainer termincontainer;
+    private Mitgliedcontainer mitgliedcontainer;
+    // pirvate Musikstueckcontainer musikstueckcontainer;
     
+    private String name;
+    private String ausrichtung;
+
+    public Musikgruppe(String name, String ausrichtung) {
+        this.name = name;
+        this.ausrichtung = ausrichtung;
+        this.termincontainer = new Termincontainer();
+        this.mitgliedcontainer = new Mitgliedcontainer();
+        //this.musikstueckcontainer = new Musikstueckcontainer();
+    }
+
+    public String getAusrichtung() {
+        return ausrichtung;
+    }
+
+    public Mitgliedcontainer getMitgliedcontainer() {
+        return mitgliedcontainer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Termincontainer getTermincontainer() {
+        return termincontainer;
+    }
+    
+   /* public Musikstueckcontainer getMusikstueckcontainer() {
+        return musikstueckcontainer;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Musikgruppe{" + "name=" + name + ", ausrichtung=" + ausrichtung + '}';
+    } 
     
 }
