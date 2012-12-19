049edf0739b21ba0984299ee424d81e896165c4d
Johannes Wawerda <johannes.wawerda@studten.tuwien.ac.at>
2012-12-19 02:08:11 +0100

    Enmus eingefuegt

---
+++ Aufgabe9/Form.java
@@ -0,0 +1,8 @@
+
+/**
+ *
+ * @author Johannes Wawerda <johannes.wawerda@student.tuwien.ac.at>
+ */
+public enum Form {
+    RUND, MOND, WEIHNACHTSMANN;
+}
---
+++ Aufgabe9/Teig.java
@@ -0,0 +1,8 @@
+
+/**
+ *
+ * @author Johannes Wawerda <johannes.wawerda@student.tuwien.ac.at>
+ */
+public enum Teig {
+    MUERB, ZIMTSTERN, SCHOKOLADE
+}
